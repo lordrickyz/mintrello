6854bd8d28a1c43d2146532829dba10e454e86de
CG <cgodlew@gmail.com>
2020-08-28 22:07:41 -0400

    Added SCSS styling to project.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
-import './styles/index.css';
+import { Switch, Route, Router } from "react-router-dom";
+import SplashPage from "../src/components/splash_page/splashPage"
+import './styles/index.scss';
 
 function App() {
   return (
+
+    // <Switch>
+    //   <Route exact path="/" component={SplashPage} />
+    // </Switch>
+
     <div className="App">
       Hello
+
+      <SplashPage />
+    
+        {/* <Route exact path="/" component={SplashPage} /> */}
+     
+
+      
     </div>
   );
 }
---
+++ src/components/splash_page/splashPage.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+
+class SplashPage extends React.Component {
+  constructor(props) {
+    super(props)
+  };
+
+  render() {
+
+
+    return (
+      <div>Test if working</div>
+    )
+
+  }
+
+
+  
+
+};
+
+
+export default SplashPage;
+
+
+
