d6598e5bf37d7d5682650655fc9083e4c2856550
lordrickyz <rickyzheng@ymail.com>
2020-08-29 14:06:59 -0400

    Add Actions for Cards

--- src/frontend/reducers/root_reducer.js
+++ src/frontend/reducers/root_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
+import modalReducer from "./modal_reducer";
 // import listReducer from "./list_reducer"
 // import cardReducer from "./card_reducer";
 
 const rootReducer = combineReducers({
   lists: {},
   cards: {},
+  modal: modalReducer,
 })
 
 export default rootReducer;
