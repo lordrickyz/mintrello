e552f1d152b209392c16ac1c33e90668534e1f00
lordrickyz <rickyzheng@ymail.com>
2020-08-28 20:13:00 -0400

    added component folders for src

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import React from 'react';
-import logo from './mintbean.png';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 import './styles/index.css';
 
 function App() {
   return (
     <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>App.js</code> is the entrypoint to your app.
-        </p>
-        <h2>HAPPY HACKING!</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      Hello
     </div>
   );
 }
