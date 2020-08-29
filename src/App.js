9993a3c14d57b13a5b4cd9e7f1e97d5767d546ff
lordrickyz <rickyzheng@ymail.com>
2020-08-28 21:35:59 -0400

    Add CardShow JSX Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import './styles/index.css';
+import CardShow from './components/card/card_show'
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div>
+      <Route path="/card" component={CardShow} />
     </div>
   );
 }
---
+++ src/components/card/card_show.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+class CardShow extends React.Component {
+
+
+
+  
+}
