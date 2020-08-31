4b2be0af204fc17118fb1fbba0635314950faf8d
LouisVelz <velazquezlouis2@gmail.com>
2020-08-31 00:28:21 -0400

    add tutorial

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -6,14 +6,18 @@ import Modal from "./modal/modal";
 import "../../styles/index.scss";
 import SplashPage from "./splash/splashPage";
 import Dashboard from "./dashboard/dashboard";
+import Navbar from "./nav/navbar";
+import Tutorial from "./tutorial/tutorial";
 
 const App = () => {
   return (
     <div>
       <Modal />
+      <Navbar />
       <Switch>
         <Route exact path="/" component={SplashPage} />
         <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact patch="/tutorial" component={Tutorial} />
       </Switch>
     </div>
   );
--- src/frontend/components/splash/splashPage.jsx
+++ src/frontend/components/splash/splashPage.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Navbar from '../nav/navbar';
+import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "../nav/navbar";
 import butler from "../../../images/Mintrello-Butler.gif";
-import splashImageOne from '../../../images/splashfiller1.png';
-import splashImageTwo from '../../../images/splashfiller2.png';
-import butlerPocket from '../../../images/buter-pocket.jpg';
-
+import splashImageOne from "../../../images/splashfiller1.png";
+import splashImageTwo from "../../../images/splashfiller2.png";
+import butlerPocket from "../../../images/buter-pocket.jpg";
 
 class SplashPage extends React.Component {
   // constructor(props) {
@@ -13,46 +12,58 @@ class SplashPage extends React.Component {
   // }
 
   render() {
-
     return (
-      <div className='splash-page-container'>
-        <header><Navbar/></header>
-        <img className ='butler-gif' src={butler} alt="The Butler is missing :O" />
+      <div className="splash-page-container">
+        {/* <header><Navbar/></header> */}
+        <img
+          className="butler-gif"
+          src={butler}
+          alt="The Butler is missing :O"
+        />
 
-        <div className='splash-page-main-words'>
+        <div className="splash-page-main-words">
           <h1>Mintrello</h1>
-          <p>The sweet Kanban board for creating, tracking, and finishing tasks</p>
-          <ul className='start-buttons-container'>
-            <li><button>Recipe Tutorial</button></li>
-            <li><button>Get Started</button></li>
+          <p>
+            The sweet Kanban board for creating, tracking, and finishing tasks
+          </p>
+          <ul className="start-buttons-container">
+            <li>
+              <button>Recipe Tutorial</button>
+            </li>
+            <li>
+              <button>Get Started</button>
+            </li>
           </ul>
         </div>
-        <ul className='splash-page-body'>
+        <ul className="splash-page-body">
           <li>
-            <h1>Track
+            <h1>
+              Track
               <br />
               <p>dsfbkjlsghjshgjghjkdg</p>
             </h1>
-            
+
             <img src={splashImageOne} alt="I'm missing :O" />
           </li>
           <li>
-          <h1>Complete
-            <br />
+            <h1>
+              Complete
+              <br />
               <p>dsfbkjlsghjshgjghjkdg</p>
-          </h1>
-          <img src={splashImageTwo} alt="I'm missing :O" />
-
-
+            </h1>
+            <img src={splashImageTwo} alt="I'm missing :O" />
           </li>
           <li>
-          <h1>Take anywhere
-            <br />
+            <h1>
+              Take anywhere
+              <br />
               <p>With Mintrello you can </p>
-          </h1>
-          <img className='butler-pocket' src={butlerPocket} alt="I'm missing :O" />
-
-
+            </h1>
+            <img
+              className="butler-pocket"
+              src={butlerPocket}
+              alt="I'm missing :O"
+            />
           </li>
         </ul>
         {/* <div className='second-test-footer'>
@@ -62,24 +73,10 @@ class SplashPage extends React.Component {
 
         </div> */}
 
-
-
-
-        <footer>
-          Whipped up by: ADD NAMES HERE
-        </footer>
+        <footer>Whipped up by: ADD NAMES HERE</footer>
       </div>
-    )
-
+    );
   }
-
-
-  
-
-};
-
+}
 
 export default SplashPage;
-
-
-
