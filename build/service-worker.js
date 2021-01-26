8abf8c5dfd21b0c93eb4240472ca1120e25f1ff6
lordrickyz <rickyzheng@ymail.com>
2021-01-25 21:39:52 -0500

    Remove Logger's in for predeploy build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,11 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-<<<<<<< HEAD
-  "./precache-manifest.30090699ffa077b4e11512f15a515f89.js"
-=======
   "./precache-manifest.3d29c41d60d6a1bbbf41b3d03a5a5822.js"
->>>>>>> ad410f174739fc0e75f32eec9fd06011a86dd24b
 );
 
 self.addEventListener('message', (event) => {
